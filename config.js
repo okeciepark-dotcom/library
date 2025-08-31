c2a87beb863f66ce770592d3c7035e99beb30f5c
okeciepark-dotcom <okeciepark@gmail.com>
2025-08-31 21:56:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 window.OP_CONFIG = {
-  orgName:    "Okęcie Park",
+  orgName:    "Okęcie Park sp. z o.o. ul. Twarda 18, 00-105 Warszawa",
   orgEmail:   "kontakt@okeciepark.pl",
   privacyUrl: "https://sites.google.com/view/okcie-park-polit-pryw/home",
   logoUrl:    "https://okeciepark-dotcom.github.io/library/logo.png"
 };
+
 
