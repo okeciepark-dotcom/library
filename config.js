42e9b0c255e8f676ab4a267db77ec408b7494391
okeciepark-dotcom <okeciepark@gmail.com>
2025-08-31 21:54:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ window.OP_CONFIG = {
   orgName:    "Okęcie Park",
   orgEmail:   "kontakt@okeciepark.pl",
   privacyUrl: "https://sites.google.com/view/okcie-park-polit-pryw/home",
-  logoUrl:    "https://lh4.googleusercontent.com/qht5n74_NyXUSl-Rc8khjV_r2HXelUsXQETIYNm9RLZCxYNlSOvci8S4tRl-RZdD_xMg4_H3aZQfQArgTP5YZaYsHOOw3HMIOoHr2mWPUuOaE-V_yAAB-XJxcxouILK5uYidbK-yQ30S6nvKwY1rDv9IvIO5aLr_sJiVQAdgXfoEU0uTbCwMLQ=w1280"
+  logoUrl:    "https://okeciepark-dotcom.github.io/library/logo.png"
 };
+
